7a620c576d307a8d8c15b36dd3c99ee35a7925c5
Andy Jones <hello@iamandyjones.co.uk>
2017-02-03 21:47:44 +0000

    Add new post on this keyowrd binding in react components

---
+++ _posts/2017-02-03-binding-react-component-methods.js
@@ -0,0 +1,43 @@
+---
+layout: post
+---
+
+React provides a default set of API methods withn a React component, such as render() and componentDidMount().
+
+When working within these default methods, React automatically binds the this keyword to the component for us.
+
+This is not the case when we define our own custom component methods.
+
+{% highlight javascript %}
+
+class Component extends React.Component {
+
+	render(){
+		// this references the component
+	}
+
+	ourOwnMethod(){
+		// this is null
+	}
+
+}
+{% endhighlight %}
+
+Therefore any time we define our own custom component methods we need to manually bind "this" to the component ourselves as follows:
+
+class Component extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.ourOwnMethod = this.ourOwnMethod.bind(this);
+	}
+
+	ourOwnMethod(){
+		// this now references the component
+	}
+
+}
+{% endhighlight %}
+
+
+
